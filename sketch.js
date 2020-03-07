d807f0471a6fd9147d4a61b11e65d07188aba74f
adam <adam@asmund.dk>
2020-03-07 19:42:32 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,28 +1,28 @@
 mode=1
 size=25
 function setup() {
-  createCanvas(800,800);
+  createCanvas(windowWidth,windowHeight);
   background(255,255,255);
 }
 
 function draw() {
   noStroke();
   fill(255,255,255);
-  rect(700,170,800,450);
+  rect(windowWidth-150,170,800,450);
   rect(0,650,800,200);
   noStroke();
   textSize(20);
   fill(50,50,50);
-  text("small",712,350);
-  text("medium",712,440);
-  text("large",712,525);
-  text("huge",712,605);
+  text("small",windowWidth-125,350);
+  text("medium",windowWidth-125,440);
+  text("large",windowWidth-125,525);
+  text("huge",windowWidth-125,605);
   text("eraser",370,750)
   text("red",118,750)
   text("blue",38,750)
   text("green",200,750)
   text("yellow",280,750)
-  text("pencil",712,265)
+  text("pencil",windowWidth-125,265)
   text("reset",460,750)
   noStroke();
   fill(0,0,200);
@@ -36,17 +36,17 @@ function draw() {
   fill(0,200,0);
   circle(220,700,50);
   fill(100,100,100);
-  circle(740,300,50);
+  circle(windowWidth-100,300,50);
   fill(100,100,100);
-  circle(740,390,50);
+  circle(windowWidth-100,390,50);
   fill(100,100,100);
-  circle(740,480,50);
+  circle(windowWidth-100,480,50);
   fill(100,100,100);
-  circle(740,560,50);
+  circle(windowWidth-100,560,50);
   fill(235,235,0);
   circle(305,700,50);
   fill(100,100,100);
-  circle(740,220,50);
+  circle(windowWidth-100,220,50);
   fill(100,100,100);
   circle(480,700,50);
    if(mouseIsPressed) {
@@ -62,28 +62,29 @@ function draw() {
        if ((mouseX-305)*(mouseX-305)+(mouseY-700)*(mouseY-700)<25*25) {
        mode=4
      }
-     if ((mouseX-740)*(mouseX-740)+(mouseY-300)*(mouseY-300)<25*25) {
+     if ((mouseX-(windowWidth-100))*(mouseX-(windowWidth-100))+(mouseY-300)*(mouseY-300)<25*25) {
        size=10
      }
-     if ((mouseX-740)*(mouseX-740)+(mouseY-390)*(mouseY-390)<25*25) {
+     if ((mouseX-(windowWidth-100))*(mouseX-(windowWidth-100))+(mouseY-390)*(mouseY-390)<25*25) {
        size=25
      }
-          if ((mouseX-740)*(mouseX-740)+(mouseY-480)*(mouseY-480)<25*25) {
+          if ((mouseX-(windowWidth-100))*(mouseX-(windowWidth-100))+(mouseY-480)*(mouseY-480)<25*25) {
        size=50
      }
-       if ((mouseX-740)*(mouseX-740)+(mouseY-560)*(mouseY-560)<25*25) {
+       if ((mouseX-(windowWidth-100))*(mouseX-(windowWidth-100))+(mouseY-560)*(mouseY-560)<25*25) {
        size=100
      }
      if ((mouseX-390)*(mouseX-390)+(mouseY-700)*(mouseY-700)<25*25) {
        mode=5
      }
-     if ((mouseX-740)*(mouseX-740)+(mouseY-200)*(mouseY-200)<25*25) {
+     if ((mouseX-(windowWidth-100))*(mouseX-(windowWidth-100))+(mouseY-200)*(mouseY-200)<25*25) {
        size=2
      }
           if ((mouseX-480)*(mouseX-480)+(mouseY-700)*(mouseY-700)<25*25) {
             fill(255,255,255);
             rect(0,0,800,800);
      }
+             
      if (mode===1){
      stroke(0,0,200);
      }
